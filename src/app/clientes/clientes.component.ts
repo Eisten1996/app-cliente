62d7efd4e4999eece3271f09210c83f621bbaa39
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-30 16:18:54 -0500

    Listando los objetos del tipo Cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,10 +1,57 @@
 import {Component, OnInit} from '@angular/core';
+import {Cliente} from './cliente';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {
+      id: 1,
+      nombre: 'Dipper',
+      apellido: 'Pines',
+      email: 'dipper@gmail.com',
+      createAt: '1996-08-13'
+    }, {
+      id: 2,
+      nombre: 'Hale',
+      apellido: 'Delhanty',
+      email: 'hdelhanty1@hp.com',
+      createAt: '2020-05-27'
+    }, {
+      id: 3,
+      nombre: 'Jesselyn',
+      apellido: 'Glazzard',
+      email: 'jglazzard2@jiathis.com',
+      createAt: '2019-10-26'
+    }, {
+      id: 4,
+      nombre: 'Celeste',
+      apellido: 'Geraudel',
+      email: 'cgeraudel3@so-net.ne.jp',
+      createAt: '2019-12-22'
+    }, {
+      id: 5,
+      nombre: 'Maggy',
+      apellido: 'Tinston',
+      email: 'mtinston4@wikispaces.com',
+      createAt: '2019-09-23'
+    }, {
+      id: 6,
+      nombre: 'Nobe',
+      apellido: 'Straniero',
+      email: 'nstraniero5@princeton.edu',
+      createAt: '2020-03-08'
+    }, {
+      id: 7,
+      nombre: 'Marybelle',
+      apellido: 'Drache',
+      email: 'mdrache6@zdnet.com',
+      createAt: '2020-07-28'
+    }
+  ];
 
   constructor() {
   }
