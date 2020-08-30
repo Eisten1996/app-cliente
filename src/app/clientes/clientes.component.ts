7fc6351d6900ed8cb9457420ab87a1db778f982f
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-30 16:23:29 -0500

    Creando archivo clientes.json.ts con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Cliente} from './cliente';
+import {CLIENTES} from './cliente.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,56 +8,13 @@ import {Cliente} from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {
-      id: 1,
-      nombre: 'Dipper',
-      apellido: 'Pines',
-      email: 'dipper@gmail.com',
-      createAt: '1996-08-13'
-    }, {
-      id: 2,
-      nombre: 'Hale',
-      apellido: 'Delhanty',
-      email: 'hdelhanty1@hp.com',
-      createAt: '2020-05-27'
-    }, {
-      id: 3,
-      nombre: 'Jesselyn',
-      apellido: 'Glazzard',
-      email: 'jglazzard2@jiathis.com',
-      createAt: '2019-10-26'
-    }, {
-      id: 4,
-      nombre: 'Celeste',
-      apellido: 'Geraudel',
-      email: 'cgeraudel3@so-net.ne.jp',
-      createAt: '2019-12-22'
-    }, {
-      id: 5,
-      nombre: 'Maggy',
-      apellido: 'Tinston',
-      email: 'mtinston4@wikispaces.com',
-      createAt: '2019-09-23'
-    }, {
-      id: 6,
-      nombre: 'Nobe',
-      apellido: 'Straniero',
-      email: 'nstraniero5@princeton.edu',
-      createAt: '2020-03-08'
-    }, {
-      id: 7,
-      nombre: 'Marybelle',
-      apellido: 'Drache',
-      email: 'mdrache6@zdnet.com',
-      createAt: '2020-07-28'
-    }
-  ];
+  clientes: Cliente[] = [];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
