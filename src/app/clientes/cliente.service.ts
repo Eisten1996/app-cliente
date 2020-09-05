3bb1a3de69738895d6df82065318541bdd41a68c
Eisten <eisten.flores@unmsm.edu.pe>
2020-09-05 18:55:45 -0500

    Escribiendo implementación crear en el cliente.service.ts y en form.component.ts

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Cliente} from './cliente';
 // import {CLIENTES} from './cliente.json';
 import {Observable, of} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,17 @@ import {HttpClient} from '@angular/common/http';
 export class ClienteService {
   private urlEndPoint = 'http://localhost:8080/api/clientes';
 
+  private httpHeaders = new HttpHeaders({'Content-type': 'application/json'});
+
   constructor(private http: HttpClient) {
   }
 
   getClientes(): Observable<Cliente[]> {
     // return of(CLIENTES);
     return this.http.get <Cliente[]>(this.urlEndPoint);
+  }
+
+  createCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
   }
 }
--- src/app/clientes/from.component.ts
+++ src/app/clientes/from.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Cliente} from './cliente';
+import {ClienteService} from './cliente.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-from',
@@ -9,15 +11,14 @@ export class FromComponent implements OnInit {
   public titulo = 'Crear Cliente';
   public cliente: Cliente = new Cliente();
 
-  constructor() {
+  constructor(private clienteService: ClienteService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
   public create(): void {
-    console.log('Clicked');
-    console.log(this.cliente);
+    this.clienteService.createCliente(this.cliente).subscribe(value => this.router.navigate(['/clientes']));
   }
 
 }
